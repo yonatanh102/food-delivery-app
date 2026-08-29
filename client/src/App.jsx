1a1b890d4da48d57c71908ec4a493fc315717086
yonatanh102 <yonatanh102@gmail.com>
2026-08-29 15:50:30 +0300

    installed vite react ESLint, axios, Tailwind CSS

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Food Delivery App is Ready! 🍔
+      </h1>
+    </div>
+  )
+}
+
+export default App
